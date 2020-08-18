fbb3706a2d969515695d7257e9dbc0359b8242f4
Rahuls-iMac <rahwinside@gmail.com>
2020-08-18 22:57:26 +0530

    Finished 1 and 2

---
+++ Asst1/Program.cs
@@ -0,0 +1,57 @@
+﻿//Determine weather conditions based on temperature
+
+//Given an integer as an input, it represents the temperature in centigrade.Determine the weather conditions based on the temperature.
+
+//Temperature< 0 then print "Freezing weather".
+//Temperature 0 - 10 then print "Very cold weather".
+//Temperature 10 - 20 then print "Cold weather".
+//Temperature 20 - 30 then print "Normal in temperature".
+//Temperature 30 - 40 then print "Its Hot".
+//Temperature >= 40 then print "Its Very Hot".
+
+//Input:
+
+//    42 
+
+//    where:
+
+//Input represents the temperature in centigrade.
+//Output:
+
+//    Its very hot
+
+
+using System;
+
+namespace Asst1
+{
+    class Program
+    {
+        static void X(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            if (t < 0) {
+                Console.WriteLine("Freezing weather");
+            }
+            else if (t >= 0 && t<10) {
+                Console.WriteLine("Very cold weather");
+            }
+            else if (t >= 10 && t < 20)
+            {
+                Console.WriteLine("Cold weather");
+            }
+            else if (t >= 20 && t < 30)
+            {
+                Console.WriteLine("Normal in temperature");
+            }
+            else if (t >= 30 && t < 40)
+            {
+                Console.WriteLine("Its Hot");
+            }
+            else
+            {
+                Console.WriteLine("Its Very Hot");
+            }
+        }
+    }
+}
---
+++ Asst1/Program2.cs
@@ -0,0 +1,85 @@
+﻿//Given an integer N as input, which represents the number of electricity units consumed.
+//Calculate the total electricity bill according to the given conditions:
+
+//For first 50 units $ 0.50/unit
+//For next 100 units $ 0.75/unit
+//For next 100 units $ 1.20/unit
+//For unit above 250 $ 1.50/unit
+//An additional surcharge of 20% is added to the bill.
+
+
+
+//Input:
+
+//    150    
+
+//    where:
+
+//Input represents the value of integer N.
+//Output:
+
+//    120.000000
+
+
+using System;
+namespace Asst1
+{
+    public class Program2
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var i = 0;
+            var sum = 0.0;
+            while (N > 0) {
+                if (i == 0) {
+                    if (N >= 50) {
+                        sum = sum + (50 * 0.5);
+                        N -= 50;
+                    }
+                    else
+                    {
+                        sum = sum + (N * 0.5);
+                        N = 0;
+                    }
+                    i += 1;
+                }
+                else if (i == 1)
+                {
+                    if (N >= 100)
+                    {
+                        sum = sum + (100 * 0.75);
+                        N -= 100;
+                    }
+                    else
+                    {
+                        sum = sum + (N * 0.75);
+                        N = 0;
+                    }
+                    i += 1;
+                }
+                else if (i == 2)
+                {
+                    if (N >= 100)
+                    {
+                        sum = sum + (100 * 1.20);
+                        N -= 100;
+                    }
+                    else
+                    {
+                        sum = sum + (N * 1.20);
+                        N = 0;
+                    }
+                    i += 1;
+                }
+                else if (i == 3) {
+                    sum = sum + (N * 1.50);
+                    N = 0;
+                    i += 1;
+                }
+            }
+            sum = sum + sum * 0.2;
+            Console.WriteLine(sum);
+        }
+    }
+}
